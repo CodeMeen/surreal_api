a23a474587ec3072d571f428d9b355c386d81c41
CodeMeen <alameenolumomi@yahoo.com>
2022-07-08 23:42:30 +0100

    commit

--- app.js
+++ app.js
@@ -14,84 +14,14 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.get('/feed', function(req, res){
-  const feed={
-
-   type:'feed',
-   posts:[
-      
-      {
-      postinfo:{
-       postid:'shgfasjdf',
-       comments:'100',
-       laughs:'100',
-       reposts:'100',
-       views:'250k',
-       commented:false,
-       laugh:false,
-       reposted:false,
-       timeofpost:'6h',
-       noofslides:1
-      },
-      userinfo:{
-       username:'Ameen',
-       userid:'gshdgksjgsf',
-       profilepic:'/profilepic.jpg'
-      },
-      slides:[
-      
-         {
-            type:'imagepost',
-            forecaption:' This is a meme,for testing and shii This is a meme,for testing and shii This is a meme,for testing and shii This is a meme,for testing and shii This is a meme,for testing and shii This is a meme,for testing and shii This is a meme,for testing and shii This is a meme,for testing and shii',
-            endcaption:false,
-            media:[
-               {
-                  imageid:'djshgjdfhgs',
-                  src:'/test.jpg',
-               }
-            ]   
-         }   
-      ],
-      read:"false"
-   }
-]
-     
-   };
-   res.send(feed);
+app.get('/getTokenData/:data/:type', function(req, res){
+   let data=req.params.data;
+   let type=req.params.type;
+ 
+  
 });
 
-app.get('/usernamecheck/:name', function(req, res){
-    const data=req.params.name;
-    res.send({available:true,query: data});
- });
 
- app.get('/emailcheck/:email', function(req, res){
-    const data=req.params.email;
-    res.send({available:true,query: data});
- });
-
- app.get('/sendresetcode/:email', function(req, res){
-   const data=req.params.email;
-   res.send({action:'success'});
-});
-
-app.get('/checkcodevalidity/:code', function(req, res){
-   const data=req.params.code;
-   res.send({action:'success'});
-});
-
-app.post('/resetpassword', function(req, res){
-   const data=req.body.password;
-   res.send({action:true});
-});
-
-app.post('/signin', function(req, res){
-   res.send({action:true,sessiondata:{username:'Ameen',userid:'useridameen',sessionid:'sessionidameen',loginstate:'active'}});
-});
-
-app.post('/signup', function(req, res){
-   res.send({action:true,sessiondata:{username:'Ameen',userid:'useridameen',sessionid:'sessionidameen',loginstate:'active'}});
-});
 
 app.use(function(req, res){
     res.type('text/plain');
