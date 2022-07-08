d023c47bcb8debe6c2a2f208b63ff4f86b3088bd
CodeMeen <alameenolumomi@yahoo.com>
2022-07-09 00:15:43 +0100

    commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 const bodyParser = require("body-parser");
 const cors= require('cors');
+const axios = require('axios');
+
+
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -17,6 +20,38 @@ app.use(express.static('public'));
 app.get('/getTokenData/:data/:type', function(req, res){
    let data=req.params.data;
    let type=req.params.type;
+
+   if(type=='id'){
+   const mapurl="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id="+data;
+   }else if(type=="name"){
+      const mapurl="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?name="+data;
+   }else if(type=="symbol"){
+      const mapurl="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol="+data;
+   }else if(type=="token_address"){
+      const mapurl="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?token_address="+data;
+   }
+
+   let response = null;
+   new Promise(async (resolve, reject) => {
+     try {
+       response = await axios.get(mapurl, {
+         headers: {
+           'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+         },
+       });
+     } catch(ex) {
+       response = null;
+       // error
+       console.log(ex);
+       reject(ex);
+     }
+     if (response) {
+       // success
+       const json = response.data;
+       console.log(json);
+       resolve(json);
+     }
+   });
  
   
 });
